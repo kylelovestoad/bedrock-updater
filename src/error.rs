172e1b5a04f915258cba414c82ff6d60a396d211
kylelovestoad <kylelovestoad+github@protonmail.com>
2024-08-23 18:23:46 -0400

    feat: attempt to use reqwest for faster grabbing of the bedrock page

--- src/error.rs
+++ src/error.rs
@@ -5,46 +5,75 @@ use std::{
     io::Error as IOError
 };
 
+use reqwest::header::InvalidHeaderValue;
+use url::ParseError;
+
 use fantoccini::error::{CmdError, NewSessionError};
 
-pub enum WebDriverError {
+#[derive(Debug)] 
+#[allow(dead_code)]
+pub enum BedrockUpdaterError {
     ConnectionError(NewSessionError),
     RustlsError(IOError),
-    InvalidCommandError(CmdError)
+    CommandFalureError(CmdError),
+    RequestError(reqwest::Error),
+    UrlConversionError(ParseError),
+    InvalidCookieError(InvalidHeaderValue),
+    DownloadError,
 }
 
-impl Display for WebDriverError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+impl Display for BedrockUpdaterError {
+    fn fmt(&self, _f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         todo!()
     }
 }
 
-impl Debug for WebDriverError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+impl Error for BedrockUpdaterError {
+    fn description(&self) -> &str {
         match self {
-            Self::ConnectionError(arg0) => f.debug_tuple("ConnectionError").field(arg0).finish(),
-            Self::RustlsError(arg0) => f.debug_tuple("RustlsError").field(arg0).finish(),
-            Self::InvalidCommandError(arg0) => f.debug_tuple("InvalidCommandError").field(arg0).finish(),
+            Self::ConnectionError(_) => "failed to connect to webdriver",
+            Self::RustlsError(_) => "rustls client failed to be created",
+            Self::CommandFalureError(_) => "invalid webdriver command",
+            Self::RequestError(_) => "request failed",
+            Self::UrlConversionError(_) => "failed to parse url",
+            Self::InvalidCookieError(_) => "invalid cookie header",
+            Self::DownloadError => "failed to download minecraft bedrock server",
         }
     }
 }
 
-impl Error for WebDriverError {}
-
-impl From<NewSessionError> for WebDriverError {
-    fn from(err: NewSessionError) -> WebDriverError {
-        WebDriverError::ConnectionError(err)
+impl From<NewSessionError> for BedrockUpdaterError {
+    fn from(err: NewSessionError) -> BedrockUpdaterError {
+        Self::ConnectionError(err)
     }
 }
 
-impl From<std::io::Error> for WebDriverError {
-    fn from(err: std::io::Error) -> WebDriverError {
-        WebDriverError::RustlsError(err)
+impl From<std::io::Error> for BedrockUpdaterError {
+    fn from(err: std::io::Error) -> BedrockUpdaterError {
+        Self::RustlsError(err)
     }
 }
 
-impl From<CmdError> for WebDriverError {
-    fn from(err: CmdError) -> WebDriverError {
-        WebDriverError::InvalidCommandError(err)
+impl From<CmdError> for BedrockUpdaterError {
+    fn from(err: CmdError) -> BedrockUpdaterError {
+        Self::CommandFalureError(err)
+    }
+}
+
+impl From<reqwest::Error> for BedrockUpdaterError {
+    fn from(err: reqwest::Error) -> BedrockUpdaterError {
+        Self::RequestError(err)
+    }
+}
+
+impl From<ParseError> for BedrockUpdaterError {
+    fn from(err: ParseError) -> BedrockUpdaterError {
+        Self::UrlConversionError(err)
+    }
+}
+
+impl From<InvalidHeaderValue> for BedrockUpdaterError {
+    fn from(err: InvalidHeaderValue) -> BedrockUpdaterError {
+        Self::InvalidCookieError(err)
     }
 }
