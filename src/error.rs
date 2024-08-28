62519618ca94a27857fcc3665be11f580ca08a7e
kylelovestoad <kylelovestoad+github@protonmail.com>
2024-08-27 20:05:47 -0400

    feat: successfully get minecraft website with faster reqwest, removing the need for webdriver

--- src/error.rs
+++ src/error.rs
@@ -1,25 +1,13 @@
 
 use std::{
     error::Error, 
-    fmt::{Debug, Display}, 
-    io::Error as IOError
+    fmt::{Debug, Display},
 };
-
-use reqwest::header::InvalidHeaderValue;
-use url::ParseError;
-
-use fantoccini::error::{CmdError, NewSessionError};
 
 #[derive(Debug)] 
 #[allow(dead_code)]
 pub enum BedrockUpdaterError {
-    ConnectionError(NewSessionError),
-    RustlsError(IOError),
-    CommandFalureError(CmdError),
     RequestError(reqwest::Error),
-    UrlConversionError(ParseError),
-    InvalidCookieError(InvalidHeaderValue),
-    DownloadError,
 }
 
 impl Display for BedrockUpdaterError {
@@ -31,49 +19,13 @@ impl Display for BedrockUpdaterError {
 impl Error for BedrockUpdaterError {
     fn description(&self) -> &str {
         match self {
-            Self::ConnectionError(_) => "failed to connect to webdriver",
-            Self::RustlsError(_) => "rustls client failed to be created",
-            Self::CommandFalureError(_) => "invalid webdriver command",
-            Self::RequestError(_) => "request failed",
-            Self::UrlConversionError(_) => "failed to parse url",
-            Self::InvalidCookieError(_) => "invalid cookie header",
-            Self::DownloadError => "failed to download minecraft bedrock server",
+            Self::RequestError(_) => "request failed"
         }
-    }
-}
-
-impl From<NewSessionError> for BedrockUpdaterError {
-    fn from(err: NewSessionError) -> BedrockUpdaterError {
-        Self::ConnectionError(err)
-    }
-}
-
-impl From<std::io::Error> for BedrockUpdaterError {
-    fn from(err: std::io::Error) -> BedrockUpdaterError {
-        Self::RustlsError(err)
-    }
-}
-
-impl From<CmdError> for BedrockUpdaterError {
-    fn from(err: CmdError) -> BedrockUpdaterError {
-        Self::CommandFalureError(err)
     }
 }
 
 impl From<reqwest::Error> for BedrockUpdaterError {
     fn from(err: reqwest::Error) -> BedrockUpdaterError {
         Self::RequestError(err)
-    }
-}
-
-impl From<ParseError> for BedrockUpdaterError {
-    fn from(err: ParseError) -> BedrockUpdaterError {
-        Self::UrlConversionError(err)
-    }
-}
-
-impl From<InvalidHeaderValue> for BedrockUpdaterError {
-    fn from(err: InvalidHeaderValue) -> BedrockUpdaterError {
-        Self::InvalidCookieError(err)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,26 @@
-use fantoccini::{
-    elements::{self, Element}, Client, ClientBuilder, Locator
+use reqwest::header::{
+    ACCEPT, ACCEPT_ENCODING, ACCEPT_LANGUAGE, CONNECTION,
 };
-use reqwest::{cookie::{self, Cookie, CookieStore, Jar}, header::{HeaderValue, InvalidHeaderValue}};
-use url::Url;
 
 use crate::error::BedrockUpdaterError;
 
-// use log::{error, info};
-
 mod error;
-
-
-
-async fn create_webdriver_client(
-    running_url: &str
-) -> Result<Client, BedrockUpdaterError> {
-    Ok(ClientBuilder::rustls()?
-        .connect(running_url)
-        .await?)
-}
 
 #[tokio::main]
 async fn main() -> Result<(), BedrockUpdaterError> {
-
     env_logger::init();
 
-    let data_platform = "serverBedrockLinux";
-
-    let css_selector = format!("a.downloadlink[data-platform={data_platform}]");
-    
-    let webdriver_client = create_webdriver_client("http://localhost:4444")
-        .await?;
-    
-    let bedrock_server_page_str = "https://www.minecraft.net/en-us/download/server/bedrock";
-
-    let bedrock_server_page_url = &bedrock_server_page_str.parse::<Url>()?;
-
-    webdriver_client.goto(bedrock_server_page_str).await?;
-
-    let user_agent = "Mozilla/5.0 (X11; Linux x86_64; rv:128.0) Gecko/20100101 Firefox/128.0";
-
-    let website_cookies = webdriver_client.get_all_cookies()
-        .await?;
-
-    let cookie_headers = website_cookies.iter()
-        .map(|cookie| HeaderValue::from_str(&cookie.to_string()))
-        .collect::<Result<Vec<_>, _>>()?;
-
-    let cookie_store = Jar::default();
-
-    cookie_store.set_cookies(&mut cookie_headers.iter(), bedrock_server_page_url);
-    
-    let element: Element = webdriver_client.find(Locator::Css(&css_selector)).await?;
-    
-    let possible_download_url = element.attr("href")
-    .await?
-    .ok_or(BedrockUpdaterError::DownloadError)?;
-
-    webdriver_client.close().await?;
+    let bedrock_server_page = "https://www.minecraft.net/en-us/download/server/bedrock";
 
     let reqwest_client = reqwest::ClientBuilder::new()
-        .user_agent(user_agent)
-        .cookie_provider(cookie_store.into())
         .build()?;
 
-    let _html = reqwest_client.get(bedrock_server_page_url.to_owned())
+    let _html = reqwest_client
+        .get(bedrock_server_page)
+        .header(ACCEPT, "text/html")
+        .header(ACCEPT_LANGUAGE, "en-US,en;q=0.5")
+        .header(ACCEPT_ENCODING, "gzip, deflate, br, zstd")
+        .header(CONNECTION, "keep-alive")
         .send()
         .await?
         .text()
