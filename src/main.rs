09e796e7bc75fb9e8a02c8b2e07a07c1020d9e91
kylelovestoad <kylelovestoad+github@protonmail.com>
2024-09-14 06:00:57 -0400

    refactor: create struct to contain common values used across functions

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
-use crate::updater::ElseErr;
-use crate::error::BedrockUpdaterError;
 use crate::args::Args;
 
 use clap::Parser;
 use error::Result;
 use std::path::Path;
 use tracing::Level;
+use updater::BedrockUpdater;
 
 mod error;
 
@@ -29,44 +28,19 @@ async fn main() -> Result<()> {
 
     let server_dir = Path::new(&args.server_dir);
 
-    let update_dir = Path::new(&args.update_dir);
-
+    let update_dir = server_dir.join(&args.update_dir);
     // The version file should be inside the server directory
-    let version_path = &server_dir.join(&args.version_file);
+    let version_path = server_dir.join(&args.version_file);
+
+    let updater = BedrockUpdater::new(
+        &client,
+        server_dir,
+        &update_dir,
+        &version_path,
+        args.set_first_version.as_deref(),
+    );
 
     loop {
-        let document = updater::fetch_document(&client).await?;
-
-        // The path part of the Url is necessary to get the filename
-        // This is so to prevent version strings from being parsed in the url if they are ever added
-        let download_link = updater::get_latest_download_link(&document).await?;
-
-        let cloned_download_link = download_link.clone();
-
-        server_dir
-            .exists()
-            .else_err(BedrockUpdaterError::NoServerPath)?;
-
-
-        let contents = String::from_utf8(std::fs::read(version_path)?).ok();
-
-        let (current, latest) = updater::get_versions(
-            cloned_download_link.path(),
-            version_path,
-            contents.as_deref(),
-            args.set_first_version.as_deref(),
-        )
-        .await?;
-
-        updater::try_update(
-            &client,
-            &current,
-            &latest,
-            download_link,
-            server_dir,
-            update_dir,
-            &version_path,
-        )
-        .await?;
+        updater.run_updater().await?
     }
 }
--- src/updater.rs
+++ src/updater.rs
@@ -1,9 +1,5 @@
 use std::{
-    collections::HashSet,
-    fs,
-    hash::Hash,
-    io::Cursor,
-    path::{Path, PathBuf},
+    collections::HashSet, fs, io::Cursor, path::Path
 };
 
 use bytes::Bytes;
@@ -14,10 +10,8 @@ use reqwest::{
     Client, RequestBuilder, Url,
 };
 use scraper::{Html, Selector};
-use tracing::{debug, info, info_span, trace};
+use tracing::{debug, info, info_span};
 use version_compare::Version;
-
-use std::fmt::Debug;
 
 use crate::error::BedrockUpdaterError;
 
@@ -76,229 +70,264 @@ impl ElseErr for bool {
     }
 }
 
-/// Gets the download link from the minecraft bedrock server download page
-/// This function's selector should be updated as the document changes
-#[tracing::instrument]
-pub async fn get_latest_download_link<'a>(document: &Html) -> Result<Url> {
-    let unparsed_selector = selector!();
-
-    let download_selector = Selector::parse(&unparsed_selector)?;
-
-    let mut select = document.select(&download_selector);
-
-    let download_element = select
-        .next()
-        .ok_or(BedrockUpdaterError::NoDownloadElement)?;
-
-    // This is to safeguard incorrect element fetching if the page changes for any reason
-    // As of now the minecraft bedrock server download page should only have one download link for each "data platform"
-    if select.next().is_some() {
-        return Err(BedrockUpdaterError::TooManyDownloadElements);
-    }
-
-    // No href element means that the element is most likely incorrect or the page has updated
-    let link = download_element
-        .attr("href")
-        .ok_or(BedrockUpdaterError::NoDownloadLinkAttr)?;
-
-    Ok(Url::parse(link)?)
-}
-
-/// Gets the latest version of the server
-/// This is fetched from the download link of the file, which contains the version string
-#[tracing::instrument(skip_all)]
-async fn get_latest_version(file_name: &str) -> Result<&str> {
-    info!("Getting latest version");
-
-    // Regex for a version string with exactly 4 parts
-    // It seems unlikely that the minecraft bedrock versioning scheme will change
-    // In the event that it does, this should be changed
-    let pattern = Regex::new(r"\d+(\.\d+){3}")?;
-
-    let version_str = pattern
-        .find(file_name)
-        .ok_or(BedrockUpdaterError::NoVersionString)?
-        .as_str();
-
-    Ok(version_str)
-}
-
-/// Gets the current version of the server
-/// For now, it does not seem like there is an easy way to check this, so it will check a version file
-/// For setup, the user must set the version once manually
-/// As new versions are downloaded, the version file will be updated
-#[tracing::instrument(skip_all)]
-async fn get_current_version<'a, T>(
-    file_path: T,
-    contents: Option<&'a str>,
-    version_to_set: Option<&'a str>,
-) -> Result<&'a str>
-where
-    T: AsRef<Path> + Debug,
-{
-    info!("Getting current version");
-    let version_res = match (version_to_set, contents) {
-        (None, None) => Err(BedrockUpdaterError::NoCurrentVersion),
-        (None, Some(contents)) => Ok(contents),
-        (Some(version), None) | (Some(version), Some(_)) => {
-            std::fs::write(&file_path, &version)?;
-
-            Ok(version)
-        }
-    };
-
-    version_res
-}
-
-/// Gets the current and latest versions in a tuple respectively
-#[tracing::instrument(skip_all)]
-pub async fn get_versions<'a, T>(
-    download_link_file: &'a str,
-    version_path: T,
-    contents: Option<&'a str>,
+pub struct BedrockUpdater<'a> {
+    client: &'a Client,
+    server_dir: &'a Path,
+    update_dir: &'a Path,
+    version_path: &'a Path,
     set_first_version: Option<&'a str>,
-) -> Result<(Version<'a>, Version<'a>)>
-where
-    T: AsRef<Path> + Debug + 'a,
-{
-
-    info!("Getting versions");
-    let latest_version_string = get_latest_version(download_link_file);
-
-    let current_version_string = get_current_version(version_path, contents, set_first_version);
-
-    let current_version = Version::from(current_version_string.await?)
-        .ok_or(BedrockUpdaterError::UnparseableVersion)?;
-    let latest_version = Version::from(latest_version_string.await?)
-        .ok_or(BedrockUpdaterError::UnparseableVersion)?;
-
-    Ok((current_version, latest_version))
 }
 
-#[tracing::instrument(skip_all)]
-async fn install_server<'a, T, U>(
-    bedrock_server_zip: &Bytes,
-    server_dir: T,
-    update_dir: U,
-    version_path: T,
-    new_version: &'a Version<'a>,
-    blacklist: &HashSet<PathBuf>,
-) -> Result<()>
-where
-    T: AsRef<Path> + Debug,
-    U: AsRef<Path> + Hash + Eq,
-{
-    info!("creating updater directory");
-    std::fs::create_dir_all(&update_dir)?;
-
-    info!("extracting updated server zip");
-    zip_extract::extract(Cursor::new(bedrock_server_zip), update_dir.as_ref(), true)?;
-
-    let entries = std::fs::read_dir(&update_dir)?;
-
-    info!("copying files");
-    for entry in entries {
-        let path = entry?.path();
-        let file_name = Path::new(path.file_name().ok_or(BedrockUpdaterError::NoFileName)?);
-        let abs_server_file_path = server_dir.as_ref().join(&file_name);
-        if !blacklist.contains(file_name) || !abs_server_file_path.exists() {
-            let abs_file_path = update_dir.as_ref().join(&path);
-            debug!("copying {abs_file_path:?} to {abs_server_file_path:?}");
-            if abs_file_path.is_file() {
-                fs::copy(&abs_file_path, &abs_server_file_path)?;
-            } else {
-                trace!("dir");
-                fs_extra::dir::create_all(&abs_server_file_path, false)?;
-                fs_extra::dir::copy(
-                    &abs_file_path,
-                    &abs_server_file_path,
-                    &CopyOptions::new().overwrite(true),
-                )?;
-            }
-        }
-    }
-
-    fs::write(version_path, new_version.as_str())?;
-
-    // Cleanup the update directory
-    info!("cleaning up");
-    std::fs::remove_dir_all(update_dir)?;
-
-    Ok(())
-}
-
-/// Attempt to get the html of the bedrock server page from an http request
-#[tracing::instrument(skip_all)]
-pub async fn fetch_document(client: &Client) -> Result<Html> {
-    info!("Attempting to fetch html document");
-    let page_request = client.get(BEDROCK_SERVER_PAGE).add_common_headers();
-
-    let html = page_request.send().await?.text().await?;
-
-    let document = Html::parse_document(&html);
-
-    Ok(document)
-}
-
-pub async fn try_update<'a, T, U>(
-    client: &Client,
-    current: &Version<'a>,
-    latest: &Version<'a>,
-    download_link: Url,
-    server_dir: T,
-    update_dir: U,
-    version_path: T,
-) -> Result<()>
-where
-    T: AsRef<Path> + Debug,
-    U: AsRef<Path> + Hash + Eq,
-{
-    let version_span = info_span!("version_check");
-    let version_guard = version_span.enter();
-    info!("Found server version: {current}");
-    info!("Found latest version: {latest}");
-
-    // The program will only try to install the server if it is not up to date
-    if current == latest {
-        info!("Server is up to date");
-        drop(version_guard);
-    } else if current > latest {
-        info!("Server is most likely a preview version, make sure you set the correct version");
-        drop(version_guard);
-    } else {
-        info!("Server is not up to date");
-        drop(version_guard);
-        let install_span = info_span!("install_phase");
-        let install_guard = install_span.enter();
-
-        let update_dir = server_dir.as_ref().join(Path::new(update_dir.as_ref()));
-
-        debug!("reading blacklist");
-        let overwrite_blacklist =
-            hashset!["permissions.json", "allowlist.json", "server.properties"]
-                .into_iter()
-                .map(|file_name| {
-                    debug!("blacklisted from overwriting: {file_name:?}");
-                    PathBuf::from(file_name)
-                })
-                .collect::<HashSet<_>>();
-
-        let download_request = client.get(download_link);
-
-        info!("downloading new server version");
-        let bedrock_server_zip: Bytes = download_request.send().await?.bytes().await?;
-
-        install_server(
-            &bedrock_server_zip,
+impl<'a> BedrockUpdater<'a> {
+    pub fn new(
+        client: &'a Client,
+        server_dir: &'a Path,
+        update_dir: &'a Path,
+        version_path: &'a Path,
+        set_first_version: Option<&'a str>,
+    ) -> Self {
+        Self {
+            client,
             server_dir,
             update_dir,
             version_path,
-            &latest,
-            &overwrite_blacklist,
-        )
-        .await?;
-        drop(install_guard);
+            set_first_version,
+        }
     }
 
-    Ok(())
+    /// Gets the download link from the minecraft bedrock server download page
+    /// This function's selector should be updated as the document changes
+    #[tracing::instrument]
+    async fn get_latest_download_link(document: &Html) -> Result<Url> {
+        let unparsed_selector = selector!();
+
+        let download_selector = Selector::parse(&unparsed_selector)?;
+
+        let mut select = document.select(&download_selector);
+
+        let download_element = select
+            .next()
+            .ok_or(BedrockUpdaterError::NoDownloadElement)?;
+
+        // This is to safeguard incorrect element fetching if the page changes for any reason
+        // As of now the minecraft bedrock server download page should only have one download link for each "data platform"
+        if select.next().is_some() {
+            return Err(BedrockUpdaterError::TooManyDownloadElements);
+        }
+
+        // No href element means that the element is most likely incorrect or the page has updated
+        let link = download_element
+            .attr("href")
+            .ok_or(BedrockUpdaterError::NoDownloadLinkAttr)?;
+
+        Ok(Url::parse(link)?)
+    }
+
+    /// Gets the current version of the server
+    /// For now, it does not seem like there is an easy way to check this, so it will check a version file
+    /// For setup, the user must set the version once manually
+    /// As new versions are downloaded, the version file will be updated
+    #[tracing::instrument(skip_all)]
+    async fn get_current_version<'b>(&self, contents: Option<&'b str>) -> Result<&'b str>
+    where
+        'a: 'b,
+    {
+        info!("Getting current version");
+        let version_res = match (self.set_first_version, contents) {
+            (None, None) => Err(BedrockUpdaterError::NoCurrentVersion),
+            (None, Some(contents)) => Ok(contents),
+            (Some(version), None) | (Some(version), Some(_)) => {
+                std::fs::write(self.version_path, &version)?;
+
+                Ok(version)
+            }
+        };
+
+        version_res
+    }
+
+    /// Gets the latest version of the server
+    /// This is fetched from the download link of the file, which contains the version string
+    #[tracing::instrument(skip_all)]
+    async fn get_latest_version(file_name: &str) -> Result<&str> {
+        info!("Getting latest version");
+
+        // Regex for a version string with exactly 4 parts
+        // It seems unlikely that the minecraft bedrock versioning scheme will change
+        // In the event that it does, this should be changed
+        let pattern = Regex::new(r"\d+(\.\d+){3}")?;
+
+        let version_str = pattern
+            .find(file_name)
+            .ok_or(BedrockUpdaterError::NoVersionString)?
+            .as_str();
+
+        Ok(version_str)
+    }
+
+    /// Gets the current and latest versions in a tuple respectively
+    #[tracing::instrument(skip_all)]
+    async fn get_versions<'b>(
+        &self,
+        download_link_file: &'b str,
+        contents: Option<&'b str>,
+    ) -> Result<(Version<'b>, Version<'b>)>
+    where
+        'a: 'b,
+    {
+        info!("Getting versions");
+        let latest_version_string = Self::get_latest_version(download_link_file);
+
+        let current_version_string = Self::get_current_version(self, contents);
+
+        let current_version = Version::from(current_version_string.await?)
+            .ok_or(BedrockUpdaterError::UnparseableVersion)?;
+        let latest_version = Version::from(latest_version_string.await?)
+            .ok_or(BedrockUpdaterError::UnparseableVersion)?;
+
+        Ok((current_version, latest_version))
+    }
+
+    /// Attempt to get the html of the bedrock server page from an http request
+    #[tracing::instrument(skip_all)]
+    async fn fetch_document(client: &Client) -> Result<Html> {
+        info!("Attempting to fetch html document");
+        let page_request = client.get(BEDROCK_SERVER_PAGE).add_common_headers();
+
+        let html = page_request.send().await?.text().await?;
+
+        let document = Html::parse_document(&html);
+
+        Ok(document)
+    }
+
+    #[tracing::instrument(skip_all)]
+    async fn install_server<'b>(
+        &self,
+        bedrock_server_zip: &'b Bytes,
+        new_version: &'b Version<'b>,
+        blacklist: &'b HashSet<&str>,
+    ) -> Result<()> {
+        info!("creating updater directory");
+        std::fs::create_dir_all(self.update_dir)?;
+
+        info!("extracting updated server zip");
+        zip_extract::extract(Cursor::new(bedrock_server_zip), self.update_dir, true)?;
+
+        let entries = std::fs::read_dir(self.update_dir)?;
+
+        info!("copying files");
+        // Start by looping through each of the files in the update dir
+        for entry in entries {
+            let path = entry?.path();
+
+            // file_name is taken from the path to compare to file names from the blacklist
+            let file_name = path
+                .file_name()
+                .ok_or(BedrockUpdaterError::NoFileName)?
+                .to_str()
+                .ok_or(BedrockUpdaterError::NoFileName)?;
+
+            // The destination is always the server's directory
+            let destination = self.server_dir.join(&file_name);
+
+            // Prevent overwrites of the files in the blacklist
+            // Don't prevent blacklisted files from being copied from update dir if they don't exist in the server dir
+            if !blacklist.contains(file_name) || !destination.exists() {
+                // The source is always the update directory
+                let source = self.update_dir.join(&path);
+                debug!("copying {source:?} to {destination:?}");
+                if source.is_file() {
+                    // When it is a file, just do a simple copy
+                    fs::copy(&source, &destination)?;
+                } else {
+                    // Recursive copy requires that all directories being copied to exist
+                    // fs_extra copy copies inside the destination directory instead of overwriting
+                    // The server directory makes more sense here
+                    fs_extra::dir::copy(
+                        &source,    
+                        self.server_dir,
+                        &CopyOptions::new().overwrite(true),
+                    )?;
+                }
+            }
+        }
+
+        // Finally, write the updated version in the version file
+        // At this point, the server is completely updated
+        fs::write(self.version_path, new_version.as_str())?;
+
+        // Cleanup the update directory
+        info!("cleaning up");
+        std::fs::remove_dir_all(self.update_dir)?;
+
+        Ok(())
+    }
+
+    async fn try_update<'b>(
+        &self,
+        current: &Version<'b>,
+        latest: &Version<'b>,
+        download_link: Url,
+    ) -> Result<()> {
+        let version_span = info_span!("version_check");
+        let version_guard = version_span.enter();
+        info!("Found server version: {current}");
+        info!("Found latest version: {latest}");
+
+        // The program will only try to install the server if it is not up to date
+        if current == latest {
+            info!("Server is up to date");
+            drop(version_guard);
+        } else if current > latest {
+            info!("Server is most likely a preview version, make sure you set the correct version");
+            drop(version_guard);
+        } else {
+            info!("Server is not up to date");
+            drop(version_guard);
+            let install_span = info_span!("install_phase");
+            let install_guard = install_span.enter();
+
+            // This will eventually be turned into an option in the struct, but for now it is hardcoded
+            info!("Reading blacklist");
+            let overwrite_blacklist =
+                hashset!["permissions.json", "allowlist.json", "server.properties"];
+
+            let download_request = self.client.get(download_link);
+
+            info!("Downloading new server version");
+            let bedrock_server_zip: Bytes = download_request.send().await?.bytes().await?;
+
+            Self::install_server(self, &bedrock_server_zip, &latest, &overwrite_blacklist).await?;
+            drop(install_guard);
+        }
+
+        Ok(())
+    }
+
+    pub async fn run_updater(&self) -> Result<()> {
+        // First get the minecraft download page html
+        let document = Self::fetch_document(self.client).await?;
+
+        // The path part of the Url is necessary to get the filename
+        // This is so to prevent version strings from being parsed in the url if they are ever added
+        let download_link = Self::get_latest_download_link(&document).await?;
+
+        // The clone is necessary
+        // The function will not be able to move download_link since it gets borrowed when calling .path()
+        let cloned_download_link = download_link.clone();
+
+        self.server_dir
+            .exists()
+            .else_err(BedrockUpdaterError::NoServerPath)?;
+
+        let contents = String::from_utf8(std::fs::read(self.version_path)?).ok();
+
+        let (current, latest) =
+            Self::get_versions(self, cloned_download_link.path(), contents.as_deref()).await?;
+
+        Self::try_update(self, &current, &latest, download_link).await?;
+
+        Ok(())
+    }
 }
